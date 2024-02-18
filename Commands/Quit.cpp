e125a7e2fc9ede6553a76154ce6c4479f4f246d7
Ahadzhan Piloian <ahadzhan.piloian@gmail.com>
2024-02-18 15:59:09 +0400

    fixes in privmsg

--- Commands/Quit.cpp
+++ Commands/Quit.cpp
@@ -10,6 +10,10 @@ void Commands::Quit(User *user, std::vector<std::string> obj)
     std::string reason = obj[0];
     if (reason[0] == ':')
         reason = reason.substr(1);
+
+    for (size_t i = 1; i < obj.size(); i++)
+        reason += " " + obj[i];
+        
     user->setQuit(true);
     user->ReplyMsg(RPL_QUIT(user->getNickname(), reason));
 }
